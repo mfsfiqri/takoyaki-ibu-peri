29b4ac52f20a6dfd4dad79daeca99d2f65abfef6
mfsfiqri <mfsfiqr@gmail.com>
2024-06-12 02:21:58 +0700

    menambahkan web takoyaki

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#b6895b",
+        dark: "#0f172a",
+      },
+    },
+  },
+  plugins: [],
+};
